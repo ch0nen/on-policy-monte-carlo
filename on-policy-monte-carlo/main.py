94011ff898796bc940f55ef0bf1fe9734ca82990
ch0nen <hi@joc.ke>
2025-02-10 10:36:41 +0100

    Moved around alot and added a Jupyter notebook

--- on-policy-monte-carlo/main.py
+++ on-policy-monte-carlo/main.py
@@ -1,5 +1,6 @@
 from matplotlib import pyplot as plt
-from utils.common import Maze
+import numpy as np
+from utils.common import Maze, plot_action_values, plot_policy
 
 def main():
     env = Maze()
@@ -12,6 +13,22 @@ def main():
 
     print(f"Observation space shape: {env.observation_space.nvec}")
     print(f"Number of actions: {env.action_space.n}")
+
+    action_values = np.zeros((5,5,4))
+
+    plot_action_values(action_values)
+
+    def policy(state, epsilon=0.2):
+        if np.random.random() < epsilon:
+            return np.random.choice(4)
+        else:
+            av = action_values[state]
+            return np.random.choice(np.flatnonzero(av == av.max()))
+
+    action = policy((0, 0), epsilon=0.5)
+    print(f"Selected action: {action}")
+
+    plot_policy(action_values, frame)
 
 if __name__ == "__main__":
     main()
